3bffcbeddb9ca38bb50ff5ae453ad58867a17a10
Shaun Altmann <shaunalt10@gmail.com>
2024-07-10 19:20:55 +1000

    created 3.3 moving the player

---
+++ 3_3_MovingThePlayer/Program.cs
@@ -0,0 +1,39 @@
+using System;
+using SplashKitSDK;
+
+namespace _3_3_MovingThePlayer
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Window w = new Window("The Player Class", 500, 400);
+            Player p = new Player(w);
+
+            // Speed Boost has been added
+
+            while (
+                    (!SplashKit.WindowCloseRequested(w))
+                    && (!p.Quit)
+            ) {
+                // process events
+                SplashKit.ProcessEvents();
+
+                // clear screen
+                SplashKit.ClearScreen(Color.White);
+
+                // handle player inputs
+                p.HandleInput();
+
+                // keep player on window
+                p.StayOnWindow(w);
+
+                // draw player
+                p.Draw();
+
+                // refresh window
+                w.Refresh(60);
+            }
+        }
+    }
+}
