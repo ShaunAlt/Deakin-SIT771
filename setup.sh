7b019a21d4a6b06ec6e3ee36574282d45482782f
Shaun Altmann <shaunalt10@gmail.com>
2024-07-10 15:29:11 +1000

    added run_program

--- setup.sh
+++ setup.sh
@@ -5,6 +5,12 @@
 # SIT771 Setup Script
 #  Contains initialization functionality which helps to simplify the creation,
 #  modification, and running of different C# programs within this repository.
+#
+# How to Use:
+# - Go to the directory where this script is located, and type the following:
+#   source ./setup.sh
+# - You will now be able to access any of the constants and functions
+#   implemented in this script.
 # =============================================================================
 
 # =============================================================================
@@ -29,7 +35,7 @@ SCRIPT_DIR=$(dirname "$( realpath "$0" )") # directory containing this script
 #
 # How to Use:
 # - Type the following line into terminal after sourcing this setup file.
-#   create_project <PROJECT_NAME>
+#   >>> create_project <PROJECT_NAME>
 # =============================================================================
 create_project() {
     echo "| Creating a New Project"
@@ -48,7 +54,7 @@ create_project() {
 
     # validate that the new folder doesn't already exist
     echo "| - Validate Folder Name"
-    if [ -d ./$1 ]; then
+    if [ -d "./$1" ]; then
         echo "| - ERROR: $1 already exists!"
         echo "| Failed"
         return 2
@@ -76,8 +82,6 @@ create_project() {
 
 # =============================================================================
 # Program Run
-# - Creates a new directory within the repository, redirects into it, and then
-#   creates a new C# project within it.
 # - If given a parameter, runs the program from the specified directory.
 #   Otherwise if no parameter is given, runs the program from the current
 #   directory.
@@ -88,19 +92,57 @@ create_project() {
 #   not specified, the current directory is used.
 #
 # Returns:
-# - None
+# - `0` : Success.
+# - `1` : ERROR - No directory specified, but current directory failed to find
+#         the program.
+# - `2` : ERROR - Directory not found.
+# - `3` : ERROR - Directory found, but failed to find the program.
+# - `4` : ERROR - Parameters were invalid.
+#
+# How to Use:
+# - Type one of the following lines into the terminal after sourcing this setup
+#   file.
+#   >>> run_program
+#   >>> run_program <DIRECTORY_NAME>
 # =============================================================================
 run_program()
 {
-    # redirect to parent directory
-    cd $SCRIPT_DIR
+    # no parameters - therefore use current directory
+    if [ $# -eq 0 ]; then
+        # validate that `Program.cs` exists
+        if [ ! -f "./Program.cs" ]; then
+            echo "ERROR: No Program.cs found in current directory"
+            return 1
+        fi
+    # directory specified - use that
+    elif [ $# -eq 1 ]; then
+        # return to base repository
+        cd $SCRIPT_DIR
 
-    # validate that the directory exists
-    if [! -d $1 ]; then
+        # validate that the directory exists
+        if [ ! -d "./$1" ]; then
+            echo "ERROR: Directory $1 not found"
+            return 2
+        fi
+
+        # validate that `Program.cs` exists in the directory
+        if [ ! -f "./$1/Program.cs" ]; then
+            echo "ERROR: No Program.cs found in directory $1"
+            return 3
+        fi
+
+        # redirect to the directory
+        cd $1
+    # invalid parameters
+    else
+        echo "ERROR: Invalid Parameters"
+        return 4
     fi
-}
 
-# skm dotnet run
+    # run the program
+    skm dotnet run
+    return 0
+}
 
 # =============================================================================
 # End of File
