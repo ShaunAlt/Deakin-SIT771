c235b201263c7c6d5ec2373128a48d9595efef25
Shaun Altmann <shaunalt10@gmail.com>
2024-07-30 16:33:40 +1000

    migrated 7.2 robots into 1 file

--- 7_2_DifferentRobots/Robot.cs
+++ 7_2_DifferentRobots/Robot.cs
@@ -84,4 +84,80 @@ namespace _7_2_DifferentRobots
             );
         }
     }
+    public class Boxy : Robot
+    {
+        // class constructor
+        public Boxy(
+                Window gameWindow,
+                Player player
+        ) : base(gameWindow, player)
+        {
+
+        }
+
+        public override void Draw()
+        {
+            double leftX = X + 12;
+            double rightX = X + 27;
+            double eyeY = Y + 10;
+            double mouthY = Y + 30;
+            SplashKit.FillRectangle(Color.Gray, X, Y, 50, 50);
+            SplashKit.FillRectangle(MainColor, leftX, eyeY, 10, 10);
+            SplashKit.FillRectangle(MainColor, rightX, eyeY, 10, 10);
+            SplashKit.FillRectangle(MainColor, leftX, mouthY, 25, 10);
+            SplashKit.FillRectangle(MainColor, leftX + 2, mouthY + 2, 21, 6);
+        }
+    }
+    public class Roundy : Robot
+    {
+        // class constructor
+        public Roundy(
+                Window gameWindow,
+                Player player
+        ) : base(gameWindow, player)
+        {
+
+        }
+
+        public override void Draw()
+        {
+            double leftX = X + 17;
+            double midX = X + 25;
+            double rightX = X + 33;
+            double midY = Y + 25;
+            double eyeY = Y + 20;
+            double mouthY = Y + 35;
+
+            SplashKit.FillCircle(Color.White, midX, midY, 25);
+            SplashKit.DrawCircle(Color.Gray, midX, midY, 25);
+            SplashKit.FillCircle(MainColor, leftX, eyeY, 5);
+            SplashKit.FillCircle(MainColor, rightX, eyeY, 5);
+            SplashKit.FillEllipse(Color.Gray, X, eyeY, 50, 30);
+            SplashKit.DrawLine(Color.Black, X, mouthY, X + 50, mouthY);
+        }
+    }
+    public class Robot_2 : Robot
+    {
+        // class constructor
+        public Robot_2(
+                Window gameWindow,
+                Player player
+        ) : base(gameWindow, player)
+        {
+
+        }
+
+        public override void Draw()
+        {
+            double leftX = X + 12;
+            double rightX = X + 27;
+            double eyeY = Y + 10;
+            double mouthY = Y + 30;
+            SplashKit.FillRectangle(Color.Black, X, Y, 50, 50);
+            SplashKit.FillRectangle(MainColor, leftX, eyeY, 10, 10);
+            SplashKit.FillRectangle(MainColor, rightX, eyeY, 10, 10);
+            SplashKit.FillRectangle(MainColor, leftX, mouthY, 25, 10);
+            SplashKit.FillRectangle(MainColor, leftX + 2, mouthY + 2, 21, 6);
+        }
+    }
 }
